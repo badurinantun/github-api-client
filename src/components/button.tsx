4e85a97be0697c6d4fc6610b6f9a8cad9fd3a6bb
Antun Badurina <antun.badurina@infinum.hr>
2020-05-02 19:48:13 +0200

    Add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { css } from '@emotion/core';
+
+import { Theme } from '../theme/theme';
+
+export const Button: React.FC<React.ComponentProps<'button'>> = (props) => (
+  <button
+    css={(theme: Theme) => css`
+      padding: ${theme.spacing(3)};
+      box-sizing: border-box;
+      cursor: pointer;
+
+      color: ${theme.colors.highlight};
+      border: 1px solid ${theme.colors.border};
+      border-radius: 3px;
+
+      font-weight: 700;
+
+      &:hover {
+        background-color: ${theme.colors.primary};
+      }
+    `}
+    {...props}
+  />
+);
